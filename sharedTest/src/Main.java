66beee2d97a7bfcbd3cfc6709889e3acf18c37b3
theCodingMan-1 <brynmorpeters@gmail.com>
2025-01-20 11:42:19 +0000

    Changed test

--- sharedTest/src/Main.java
+++ sharedTest/src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Bryn! Can you see this?");
+        System.out.println("I can see it!!");
     }
 }
